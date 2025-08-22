a371203d75c9056ddde6b15d4909654d2c0ec615
Thushara <62303832+thushmanoj@users.noreply.github.com>
2025-08-22 10:24:31 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -23,11 +23,11 @@ struct Vital {
 bool vitalRangeCheck(float val, Vital* v) {
     return (val < v->min || val > v->max);
 }
-bool vitalMinCheck(float val, Vital * v){ 
+bool vitalMinCheck(float val, Vital * v){
     return (val< v->min);
 }
 
- vector<Vital> vitals = {
+vector<Vital> vitals = {
 {95, 102, vitalRangeCheck, "Temperature is critical!"},
 {60, 100, vitalRangeCheck, "Pulse Rate is out of range!"},
 {90, 100, vitalMinCheck, "Oxygen Saturation out of range!"}
